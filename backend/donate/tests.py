cbd6b5a034da3d1098af688df43c0f8e6b9247ac
Alexander Nelson <alexn1336@gmail.com>
2018-08-06 16:43:07 -0400

    Igonre pytest cache

--- backend/donate/tests.py
+++ backend/donate/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your tests here.
 import pytest
 
-@pytest.mark.django_db(transaction=True)
+@pytest.mark.django_db
 def test_my_user():
     qs = User.objects.all()
     assert not qs.exists()
