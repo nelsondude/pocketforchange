70882acc02826e9dff0d30e1de11ded0a5327940
Alexander Nelson <alexn1336@gmail.com>
2018-08-07 16:29:59 -0400

    Render bootstrap form with plaid auth

--- src/containers/Donate/Registration/Registration.js
+++ src/containers/Donate/Registration/Registration.js
@@ -1,17 +1,23 @@
-import React, { Fragment } from 'react';
-import axios from 'axios-instance';
+import React, {Fragment} from 'react';
+import PlaidLink from 'react-plaid-link'
 
 class Registration extends React.Component {
+  //User name: user_good
+  //Pass word: pass_good
+  //Pin: credential_good
 
   submitHandler = (event) => {
     event.preventDefault();
-    axios.get('/donate/users/')
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    alert('form submitted');
+  };
+
+  handleOnSuccess = (token, metadata) => {
+    console.log(token, metadata);
+    alert('success!');
+  };
+
+  handleOnExit = () => {
+    alert('exiting ...');
   };
 
   render() {
@@ -19,10 +25,31 @@ class Registration extends React.Component {
       <Fragment>
         <h1>Register to be a Donator!!!</h1>
         <form action="" onSubmit={this.submitHandler}>
-          <label htmlFor="name">Name</label>
-          <input id="name" type="text"/>
-          <input type="submit" value="Submit" />
+          <div className="form-group">
+            <label htmlFor="name">Full Name</label>
+            <input type="text" className="form-control" id="name"/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                   placeholder="Enter email"/>
+              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" placeholder="Password"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
         </form>
+
+        <PlaidLink
+          clientName={'Pocket For Change'}
+          env={process.env.REACT_APP_PLAID_ENV}
+          publicKey={process.env.REACT_APP_PLAID_PUBLIC_KEY}
+          product={["auth", "transactions"]}
+          onExit={this.handleOnExit}
+          onSuccess={this.handleOnSuccess}
+        >Open Link and connect to your bank!</PlaidLink>
       </Fragment>
     )
   }
