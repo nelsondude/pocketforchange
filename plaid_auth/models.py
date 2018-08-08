411d19dc0f576680be7fbd88346cce68c38048cb
Alexander Nelson <alexn1336@gmail.com>
2018-08-08 15:49:08 -0400

    Add custom user and plaid item models

--- plaid_auth/models.py
+++ plaid_auth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class PlaidItem(models.Model):
+    access_token = models.CharField(max_length=100)
+    item_id = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='plaid_items')
