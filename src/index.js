c8f31d59bcc14cbde7b42aaae76b3a1e19c12fed
Alexander Nelson <alexn1336@gmail.com>
2018-08-09 17:48:17 -0400

    Setup react redux with dependencies

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App/App';
-import {BrowserRouter} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import store, {history} from './store'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router'
 
 const app = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App/>
+    </ConnectedRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
