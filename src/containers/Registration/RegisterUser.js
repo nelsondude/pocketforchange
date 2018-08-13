3b4a2eec6505dc6b2860d27de56063e6af176b52
Alexander Nelson <alexn1336@gmail.com>
2018-08-13 14:38:04 -0400

    Make register form inputs reusable

--- src/containers/Registration/RegisterUser.js
+++ src/containers/Registration/RegisterUser.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import axios from 'axios-instance';
 import {connect} from 'react-redux';
+import {Input} from 'components';
 
 class RegisterUser extends React.Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
-    errors: [],
+    inputs: [
+      {
+        name: 'username',
+        placeholder: 'John Smith',
+        label: 'Full Name'
+      },
+      {
+        name: 'email',
+        placeholder: 'john@smith.com',
+        label: 'Email Address',
+        type: 'email'
+      },
+      {
+        name: 'password1',
+        placeholder: 'Password',
+        label: 'Password',
+        type: 'password'
+      },
+      {
+        name: 'password2',
+        placeholder: 'Confirm Password',
+        label: 'Password Again',
+        type: 'password'
+      }
+    ]
   };
 
   submitHandler = (event) => {
@@ -33,48 +55,9 @@ class RegisterUser extends React.Component {
         <div className="row">
           <div className="col-xs-12">
             <form action="" onSubmit={this.submitHandler}>
-              <div className="form-group">
-                <label htmlFor="name">Full Name</label>
-                <input
-                  required
-                  className="form-control"
-                  id="username"
-                  name="username"
-                  placeholder="John Smith"/>
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email address</label>
-                <input
-                  required
-                  type="email"
-                  className="form-control"
-                  id="email"
-                  name="email"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter email"/>
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.
-                </small>
-              </div>
-              <div className="form-group">
-                <label htmlFor="password1">Password</label>
-                <input
-                  required
-                  type="password"
-                  className="form-control"
-                  id="password1"
-                  name="password1"
-                  placeholder="Password"/>
-              </div>
-              <div className="form-group">
-                <label htmlFor="password2">Password Confirmation</label>
-                <input
-                  required
-                  type="password"
-                  className="form-control"
-                  id="password2"
-                  name="password2"
-                  placeholder="Password Confirmation"/>
-              </div>
+              {this.state.inputs.map(options => (
+                <Input {...options} />
+              ))}
               <button type="submit" className="btn btn-primary">Register</button>
             </form>
           </div>
