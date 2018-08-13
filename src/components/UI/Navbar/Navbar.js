d8a4f67e95b44e314142ea1f2b08aaaf0860405f
Alexander Nelson <alexn1336@gmail.com>
2018-08-13 14:14:30 -0400

    Connect navbar to react redux

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {MenuItem, Nav, Navbar, NavDropdown, NavItem} from 'react-bootstrap';
+import {Nav, Navbar, NavItem} from 'react-bootstrap';
 import {Link} from 'react-router-dom'
 import {LinkContainer} from 'react-router-bootstrap';
-import {withRouter} from "react-router";
+import {connect} from 'react-redux';
 import './Navbar.css';
 
-class navbar extends React.Component {
+class CustomNavbar extends React.Component {
 
   state = {
     loggedIn: localStorage.getItem('token') !== ''
@@ -16,7 +16,7 @@ class navbar extends React.Component {
       localStorage.removeItem('token');
       this.setState({loggedIn: false})
     } else {
-      this.props.history.push('/login');
+      // this.props.history.push('/login');
     }
   };
 
@@ -42,13 +42,11 @@ class navbar extends React.Component {
                   Register
                 </NavItem>
               </LinkContainer>
-              <NavDropdown eventKey={3} title="Drop" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider/>
-                <MenuItem eventKey={3.4}>Separated link</MenuItem>
-              </NavDropdown>
+              <LinkContainer to={'/dashboard/'}>
+                <NavItem eventKey={4} href="#">
+                  Dashboard
+                </NavItem>
+              </LinkContainer>
             </Nav>
             <Nav pullRight>
               <NavItem eventKey={1} onClick={this.loggedInStatusHandler}>
@@ -62,4 +60,12 @@ class navbar extends React.Component {
   }
 }
 
-export default withRouter(navbar);
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  ...dispatch
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomNavbar);
