935bd89e26b33206852ca5597f260f1f9d9b937f
Alexander Nelson <alexn1336@gmail.com>
2018-08-14 11:53:20 -0400

    Modify axios to update auth header

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
-import store from 'store';
 
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 
-let axios_args = {
+export default axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://0.0.0.0:8000',
-};
-
-const state = store.getState();
-if (state.auth.loggedIn) {
-  axios_args['headers'] = {'Authorization': 'JWT ' + state.auth.token}
-}
-
-export default axios.create(axios_args);
+});
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 import {LOGIN, LOGOUT} from "./actions";
 import {combineReducers} from "redux";
+import axios from 'axios-instance';
 
 const initialState = {
   loggedIn: localStorage.getItem('token') !== null,
@@ -9,9 +10,11 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case LOGIN:
+      axios.defaults.headers.common['Authorization'] = 'JWT ' + action.token;
       localStorage.setItem('token', action.token);
       return {...state, loggedIn: true, token: action.token};
     case LOGOUT:
+      delete axios.defaults.headers.common["Authorization"];
       localStorage.removeItem('token');
       return {...state, loggedIn: false, token: null};
     default:
