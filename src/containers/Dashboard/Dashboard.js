3cb57557433ee08d85b5dd53768c95fed7ba4700
Alexander Nelson <alexn1336@gmail.com>
2018-08-14 19:23:33 -0400

    Refactor dashboard with react table

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,43 +1,74 @@
-import React from 'react';
-import axios from 'axios-instance';
+import React, {Fragment} from 'react';
+import ReactTable from 'react-table'
 import {connect} from 'react-redux';
+import {Link} from "react-router-dom";
 
 class Dashboard extends React.Component {
 
   state = {
-    transactions: []
+    organizations: {
+      data: [
+        {
+          name: 'ALS Foundation',
+          img: 'url here',
+          amount_donated: 100.52,
+          payment_plan_description: '1 dollar per day'
+        },
+        {
+          name: 'Red Cross',
+          img: 'url here',
+          amount_donated: 50.52,
+          payment_plan_description: 'round up to the nearest dollar per purchase'
+        },
+        {
+          name: 'Leukemia Foundation',
+          img: 'url here',
+          amount_donated: 1040.42,
+          payment_plan_description: '$10 a week'
+        },
+      ],
+      columns: [
+        {Header: 'Name', accessor: 'name'},
+        {Header: 'Amount Donated', accessor: 'amount_donated'},
+        {Header: 'Payment Plan', accessor: 'payment_plan_description'}
+      ]
+    },
+    todo: [{
+      label: 'SETUP BANK ACCOUNT',
+      url: '/add-bank-account'
+    }]
+
   };
 
   componentDidMount() {
-    axios.get('/plaid/transactions/')
-      .then(res => {
-        console.log(res);
-        this.setState({transactions: res.data.transactions})
-      })
+
   }
 
   render() {
     return (
-      <div>
+      <Fragment>
         <div className="row">
-          <div className="col-xs-6">
-            <h3 className="text-center">Account Details</h3>
-          </div>
-          <div className="col-xs-6">
-            <h3 className="text-center">Recent Activity</h3>
+          <div className="col-xs-12">
+            <h3 className="text-center">My Organizations</h3>
+            <ReactTable
+              showPagination={false}
+              showPageJump={false}
+              pageSize={5}
+              data={this.state.organizations.data}
+              columns={this.state.organizations.columns}/>
           </div>
         </div>
         <div className="row">
-          <h3 className="text-center">All Recent Transactions</h3>
-          <ul className="list-group">
-            {this.state.transactions && this.state.transactions.length > 0 ? this.state.transactions.map((value, i) => (
-              <li className="list-group-item" key={i}>
-                {value.name} | <b>${value.amount.toFixed(2)}</b>
-              </li>
-            )) : <h1>Loading ...</h1>}
-          </ul>
+          <div className="col-sm-6">
+            <h3>Outstanding Items to Complete</h3>
+            <div className="list-group">
+              {this.state.todo.map((item, i) => (
+                <Link className={'list-group-item list-group-item-action'} key={i} to={item.url}>{item.label}</Link>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      </Fragment>
     )
   }
 }
