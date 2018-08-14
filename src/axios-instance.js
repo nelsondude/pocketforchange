ad132decff17ee956d6aa629ea66bf1a27a1f61d
Alexander Nelson <alexn1336@gmail.com>
2018-08-14 12:02:59 -0400

    Set default header on initial load

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 
+if (localStorage.getItem('token') !== null) {
+  axios.defaults.headers.common['Authorization'] = 'JWT ' + localStorage.getItem('token');
+}
+
 export default axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL || 'http://0.0.0.0:8000',
 });
